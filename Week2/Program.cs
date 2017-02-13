0af56ca1c6939e91e934d565645c08faa2ba672f
nicolaivicol <nicolaivicol@gmail.com>
2017-02-14 01:05:36 +0200

    Add files via upload

---
+++ Week2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo1_Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read file with integers
+            string filename = @"E:\courses\algorithms I\week 2\QuickSort.txt";
+            //string filename = @"E:\courses\algorithms I\week 1\hw\IntegerArrayCheck_15.txt";
+            List<int> lst = new List<int>();
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                while (!sr.EndOfStream)
+                {
+                    int x = Convert.ToInt32(sr.ReadLine());
+                    lst.Add(x);
+                }
+            }
+            //List<int> lst = 
+
+            int cntFirst = QuickSort.DoQuickSortCnt(CopyOf(lst), QuickSort.Pivot.First);
+            int cntLast = QuickSort.DoQuickSortCnt(CopyOf(lst), QuickSort.Pivot.Last);
+            int cntMedian = QuickSort.DoQuickSortCnt(CopyOf(lst), QuickSort.Pivot.Median);
+
+            Console.WriteLine("First: {0}", cntFirst);
+            Console.WriteLine("Last: {0}", cntLast);
+            Console.WriteLine("Median: {0}", cntMedian);
+            Console.ReadLine();
+        }
+
+        public static List<int> CopyOf(List<int> lst)
+        {
+            List<int> lstcopy = new List<int>();
+            for (int i = 0; i < lst.Count; i++)
+            {
+                lstcopy.Add(lst[i]);
+            }
+            return lstcopy;
+        }
+    }
+}
---
+++ Week2/QuickSort.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo1_Week2
+{
+    public class QuickSort
+    {
+        public enum Pivot
+        {
+            First,
+            Last,
+            Median
+        }
+
+        public static void DoQuickSort(List<int> lst, Pivot piv)
+        {
+            DoQuickSort(ref lst, 0, lst.Count - 1, piv);
+        }
+        public static int DoQuickSortCnt(List<int> lst, Pivot piv)
+        {
+            int cnt = 0;
+            DoQuickSort(ref lst, ref cnt, 0, lst.Count - 1, piv);
+            return cnt;
+        }
+
+        private static void DoQuickSort(ref List<int> lst, int l, int r, Pivot piv)
+        {
+            if ((r - l) < 1)
+                return;
+            int p = ChoosePivot(lst, l, r, piv);
+            int i = Partition(ref lst, l, r);
+            DoQuickSort(ref lst, l, i - 1, piv);
+            DoQuickSort(ref lst, i + 1, r, piv);
+        }
+        private static void DoQuickSort(ref List<int> lst, ref int cnt, int l, int r, Pivot piv)
+        {
+            if ((r - l) < 1)
+                return;
+            int p = ChoosePivot(lst, l, r, piv);
+            int i = Partition(ref lst, l, r);
+            cnt = cnt + (r - l);
+            DoQuickSort(ref lst, ref cnt, l, i - 1, piv);
+            DoQuickSort(ref lst, ref cnt, i + 1, r, piv);
+        }
+
+        private static int ChoosePivot(List<int> lst, int l, int r, Pivot piv)
+        {
+            int piv_i = l;
+            if (piv == Pivot.First)
+                piv_i = l;
+            else if (piv == Pivot.Last)
+                piv_i = r;
+            else if (piv == Pivot.Median)
+            {
+                if ((lst[r] <= lst[l] && lst[l] <= lst[(l + r) / 2]) || (lst[r] >= lst[l] && lst[l] >= lst[(l + r) / 2]))
+                    piv_i = l;
+                else if ((lst[l] <= lst[r] && lst[r] <= lst[(l + r) / 2]) || (lst[l] >= lst[r] && lst[r] >= lst[(l + r) / 2]))
+                    piv_i = r;
+                else
+                    piv_i = (l + r) / 2;
+            }
+            Swap(ref lst, l, piv_i);
+            return lst[l];
+        }
+        private static int Partition(ref List<int> lst, int l, int r)
+        {
+            int p = lst[l];
+            int i = l + 1;
+            for (int j = l + 1; j <= r; j++)
+            {
+                if (lst[j] < p)
+                {
+                    Swap(ref lst, i, j);
+                    i++;
+                }
+            }
+            Swap(ref lst, l, i - 1);
+            return i - 1;
+        }
+        private static void Swap(ref List<int> lst, int i1, int i2)
+        {
+            int misc = lst[i1];
+            lst[i1] = lst[i2];
+            lst[i2] = misc;
+        }
+
+    }
+}
