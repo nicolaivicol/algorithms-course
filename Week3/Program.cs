2019dbe7a692505267ac29fe07fad46d43a1bc0b
nicolaivicol <nicolaivicol@gmail.com>
2017-02-23 14:55:17 +0200

    Update Program.cs

--- Week3/Program.cs
+++ Week3/Program.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.Diagnostics;
 
 namespace Algo1_Week3
 {
@@ -11,11 +7,17 @@ namespace Algo1_Week3
     {
         static void Main(string[] args)
         {
-            string filename = @"E:\courses\algorithms I\week 3\kargerMinCut.txt";
+            string filename = @"D:\courses\algorithms 1\kargerMinCut.txt";
             Graph graph = new Graph(filename);
-
-            
-
+            Console.WriteLine("Karger Min Cut");
+            Console.Write("Give N of cut attempts: ");
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int iterN = int.Parse(Console.ReadLine());
+            int mincut = graph.FindMinCut(iterN);
+            stopWatch.Stop();
+            Console.WriteLine("min cut: {0}   |  sec/cut: {1:0.0000}", mincut, stopWatch.Elapsed.TotalSeconds/ iterN);
+            Console.ReadLine();
         }
     }
 }
